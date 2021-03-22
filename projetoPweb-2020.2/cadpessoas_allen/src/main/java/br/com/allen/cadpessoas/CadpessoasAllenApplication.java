9cfd767b6d1728feb58bbd5966840785f795ff29
Allen Bruno Cardoso da Silva <brunocardo11@gmail.com>
2021-03-22 18:32:30 -0300

    Inicio do projeto Pweb 2020.2

---
+++ projetoPweb-2020.2/cadpessoas_allen/src/main/java/br/com/allen/cadpessoas/CadpessoasAllenApplication.java
@@ -0,0 +1,14 @@
+package br.com.allen.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasAllenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasAllenApplication.class, args);
+	}
+
+}
+
---
+++ projetoPweb-2020.2/cadpessoas_allen/src/test/java/br/com/allen/cadpessoas/CadpessoasAllenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allen.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasAllenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
