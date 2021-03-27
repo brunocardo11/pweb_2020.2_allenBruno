4d2db2cf8c759565ff10ab5176de63231eaaa808
Allen Bruno Cardoso da Silva <brunocardo11@gmail.com>
2021-03-27 18:28:58 -0300

    Linkando todas as paginas do site

--- projetoPweb-2020.2/cadpessoas_allen/src/main/java/br/com/allen/cadpessoas/controllers/PessoaController.java
+++ projetoPweb-2020.2/cadpessoas_allen/src/main/java/br/com/allen/cadpessoas/controllers/PessoaController.java
@@ -1,10 +1,10 @@
 package br.com.allen.cadpessoas.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.allen.cadpessoas.model.Pessoa;
@@ -14,13 +14,25 @@ import br.com.allen.cadpessoas.repositories.PessoaRepository;
 public class PessoaController {
 
 	@Autowired
-	PessoaRepository pessoaRepo;
+	private PessoaRepository pessoaRepo;
 
 	@GetMapping("/listarPessoas.html")
 	public ModelAndView listarPessoas() {
-		List<Pessoa> lista = pessoaRepo.findAll();
+		Iterable<Pessoa> lista = pessoaRepo.findAll();
 		ModelAndView mav = new ModelAndView("/listarPessoas");
 		mav.addObject("pessoas", lista);
 		return mav;
 	}
+	
+	@RequestMapping(value="/adicionarPessoa.html", method=RequestMethod.GET)
+	public String cad() {
+		return"/adicionarPessoa.html";
+	}
+	
+	@RequestMapping(value="/adicionarPessoa.html", method=RequestMethod.POST)
+	public String cad(Pessoa pessoa) {
+		pessoaRepo.save(pessoa);
+		return"/adicionarPessoa.html";
+	}
+	
 }
