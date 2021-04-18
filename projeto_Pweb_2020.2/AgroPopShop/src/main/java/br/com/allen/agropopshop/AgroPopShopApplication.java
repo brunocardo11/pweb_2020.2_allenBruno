4dc0906c07d62dc12a7c7926eb49c294105fdbe3
Allen Bruno Cardoso da Silva <brunocardo11@gmail.com>
2021-04-18 11:39:03 -0300

    Commit inicial do projeto

---
+++ projeto_Pweb_2020.2/AgroPopShop/src/main/java/br/com/allen/agropopshop/AgroPopShopApplication.java
@@ -0,0 +1,13 @@
+package br.com.allen.agropopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroPopShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroPopShopApplication.class, args);
+	}
+
+}
---
+++ projeto_Pweb_2020.2/AgroPopShop/src/test/java/br/com/allen/agropopshop/AgroPopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allen.agropopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroPopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
