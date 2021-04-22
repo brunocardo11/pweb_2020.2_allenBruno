4736b8f6da9f26b1802d7a1da82c9706f14d08b5
Allen Bruno Cardoso da Silva <brunocardo11@gmail.com>
2021-04-22 18:53:29 -0300

    Completando CRUD do cliente

--- projeto_Pweb_2020.2/AgroPopShop/src/main/java/br/com/allen/agropopshop/controllers/ClienteController.java
+++ projeto_Pweb_2020.2/AgroPopShop/src/main/java/br/com/allen/agropopshop/controllers/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +28,7 @@ public class ClienteController {
 		return "index.html";
 	}
 	
+	//Retorna a lista de clientes
 	@GetMapping("/listarClientes")
 	public ModelAndView listarclientes() {
 		List<Cliente> lista = clienteRepo.findAll();
@@ -35,18 +37,45 @@ public class ClienteController {
 		return ModelAndView;
 	}
 	
-	
+	//Retorna o formulário de cadastro de clientes
 	@GetMapping("/adicionarCliente")
-	public ModelAndView formAdicionarPessoa() { 
+	public ModelAndView formAdicionarCliente() { 
 		ModelAndView mav = new ModelAndView("user/adicionarCliente");
 		mav.addObject(new Cliente());
 		return mav;
 	}
 	
+	//Adiciona pessoa
 	@PostMapping("/adicionarCliente")
-	public String adicionarPessoa(Cliente c) {
+	public String adicionarCliente(Cliente c) {
 		this.clienteRepo.save(c);
 		return "redirect:/listarClientes";
+	}
+	
+	//Remover cliente
+	@GetMapping("/remover/{id}")
+	public ModelAndView removerCliente(@PathVariable("id") long id){
+		Cliente aRemover = clienteRepo.findById(id)
+		.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+		
+		clienteRepo.delete(aRemover);
+		return new ModelAndView("redirect:/listarClientes");
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView editarCliente (@PathVariable("id") long id) {
+		Cliente cliente = clienteRepo.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+		
+		ModelAndView ModelAndView = new ModelAndView("user/editarCliente");
+		ModelAndView.addObject(cliente);
+		return ModelAndView;
+	}
+	
+	@PostMapping("/editar/{id}")
+	public ModelAndView editarPessoa(@PathVariable("id")long id, Cliente cliente) {
+		this.clienteRepo.save(cliente);
+		return new ModelAndView( "redirect:/listarClientes");
 	}
 
 }
